fd3c78942b9acfe5cada7c158ac60c80bd301be9
MIT leon <leonc@mit.edu>
2020-10-07 19:28:39 -0400

    add button works to add timer

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import '../sass/styles.scss';
 const Timer = require('../js/time.js');
 
 
+var addbutton = document.getElementById("addbutton");
+addbutton.addEventListener("click", () => {
+  new Timer("New").setTimer(0, 0, 0, 0);
+})
+
+
 
 new Timer("Task").setTimer(0, 5, 0, 0);
 new Timer("Duty").setTimer(0, 10, 0, 0);
