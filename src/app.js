4b27aded53409a6aab2bc5d46733ee4732e00ade
Leon Cheng <57leonardo@gmail.com>
2020-10-18 16:53:35 -0400

    add comments

--- src/app.js
+++ src/app.js
@@ -4,15 +4,19 @@ import '../sass/styles.scss';
 
 const Timer = require('../js/time.js');
 
-
+// When you click on the + button, it initializes a new Timer object
 var addbutton = document.getElementById("addbutton");
 addbutton.addEventListener("click", () => {
   new Timer("New").setTimer(0, 0, 0, 0);
 })
 
 
+// Start out with these timers by default
+var timersList = [];
+timersList.push(new Timer("Task").setTimer(0, 5, 0, 0));
+timersList.push(new Timer("Duty").setTimer(0, 10, 0, 0));
+timersList.push(new Timer("Work").setTimer(0, 30, 0, 0));
+timersList.push(new Timer("Foo").setTimer(1, 0, 0, 0));
 
-new Timer("Task").setTimer(0, 5, 0, 0);
-new Timer("Duty").setTimer(0, 10, 0, 0);
-new Timer("Work").setTimer(0, 30, 0, 0);
-new Timer("Foo").setTimer(1, 0, 0, 0);
+// Bigtimer by default
+var bigtimer = timersList[0];
